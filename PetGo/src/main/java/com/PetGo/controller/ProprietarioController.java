300fa00a89e6f788b189e74ccf6a51d64695ca85
pedrosamegima <136722484+pedrosamegima@users.noreply.github.com>
2024-06-06 15:41:37 -0300

    Update ProprietarioController.java

--- PetGo/src/main/java/com/PetGo/controller/ProprietarioController.java
+++ PetGo/src/main/java/com/PetGo/controller/ProprietarioController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/proprietario")
 public class ProprietarioController {
 	private final ProprietarioService proprietarioService;
